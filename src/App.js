c920d49cd69795134db6bf08fed4de15ff6c845c
tharindu125 <nawanjanaenda@gmail.com>
2022-09-24 22:01:10 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbarcomp from './components/Navbarcomp';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbarcomp/>
+      <Home/>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export default class Home extends Component {
   render() {
     return (
-      <div> Home </div>
+      <section>
+        <div className='reg'>
+            <div className='c-1'>
+                <h1 className='text'>Join With Us</h1>
+
+                <span>Experience our services</span>
+
+            </div>
+
+            <div className='col-2'>
+                
+            </div>
+        </div>
+    </section>
     );
   }
 }
